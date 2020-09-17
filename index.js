58a22dd59de95f1768b8b244feb91eae56dc74de
Remus Calugarescu <remcalu@outlook.com>
2020-09-17 17:31:20 -0400

    Save data for when bot restarts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const readline = require('readline');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -7,12 +8,22 @@ client.commands = new Discord.Collection();
 const prefix = '`';
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
+//fs.writeFile("test.txt", backupArray, (err) => {}); 
+
+
+
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
 function fancyTime(today) {
+    let year = today.getFullYear();
+    
+    let month = today.getMonth()+1;
+
+    let day = today.getDate();
+
     let hour = today.getHours();
     if (hour <= 9) {
         hour = '0' + hour;
@@ -27,7 +38,7 @@ function fancyTime(today) {
     if (second <= 9) {
         second = '0' + second;
     }
-    return(hour + ':' + minute + ':' + second);
+    return(year + "-" + month + "-" + day + "   " + hour + ':' + minute + ':' + second);
 }
 
 let list;
@@ -40,8 +51,19 @@ client.on('ready', () => {
     console.log('Online at ' + time);
     console.log('--------------------------------------');
     list = client.users.cache.array();
+    list.sort();
     for (let i = 0; i < list.length; i++) {
         firstOffline.push(1);
+    }
+
+    // When discord restarts, read from the files
+    let timeListBackup = fs.readFileSync('timeListBackup.txt').toString().split(",");
+    for(i in timeListBackup) {
+        timeList[i] = timeListBackup[i];
+    }
+    let firstOfflineBackup = fs.readFileSync('firstOfflineBackup.txt').toString().split(",");
+    for(i in firstOfflineBackup) {
+        firstOffline[i] = firstOfflineBackup[i];
     }
 
     setInterval(function() {
@@ -50,14 +72,19 @@ client.on('ready', () => {
 
         for (let i = 0; i < list.length; i++) {
             if (!(list[i].presence.status == "offline")) {
+                // If the person isn't online update array to a green circle and mark that they haven't gone offline
                 timeList[i] = ":green_circle:";
                 firstOffline[i] = 1;
-            }
-            else if ((list[i].presence.status == "offline") && (firstOffline[i] == 1)) {
+            } else if ((list[i].presence.status == "offline") && (firstOffline[i] == 1)) {
+                // If the person is offline and its the first time they're offline, 
+                // Update array to the current time and mark that they've gone offline for the first time
                 timeList[i] = time;
                 firstOffline[i] = 0;
             }
         }
+        // Backup the values to a text file
+        fs.writeFile("timeListBackup.txt", timeList, (err) => {}); 
+        fs.writeFile("firstOfflineBackup.txt", firstOffline, (err) => {}); 
     }, 1000);
 });
 
